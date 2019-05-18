14d6af76499a9d43104a4210de16dd9558c60d9b
Renato Querino <rquerino@e1z3r2p23.42.us.org>
2019-05-18 12:55:13 -0700

    file creation

---
+++ get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rquerino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 11:55:17 by rquerino          #+#    #+#             */
+/*   Updated: 2019/05/18 12:55:01 by rquerino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+
+
+
+
+/*
+ ** Reads a file line by line.
+ ** First parameter is the file descriptor used to read, second parameter 
+ ** is the adress of pointer to a character that will be used to save
+ ** the line read from the file descriptor. 
+ ** Returns -1 error, 0 rear completed, 1 line read. 
+ ** Functions allowed: libft, read(), malloc() and free().
+*/
+
+int		get_next_line(const int fd, char **line)
+{
+
+
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rquerino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 12:39:37 by rquerino          #+#    #+#             */
+/*   Updated: 2019/05/18 12:41:09 by rquerino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <libft.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif

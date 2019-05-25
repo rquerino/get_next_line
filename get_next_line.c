4ce293f8408c877a823ddff57be82a3bbe129050
Renato Querino <rquerino@e1z3r2p23.42.us.org>
2019-05-25 14:04:46 -0700

    alteracoes no .c

--- get_next_line.c
+++ get_next_line.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <unistd.h>
+#include "get_next_line.h"
 
 /* 
  ** pseudocode: 
@@ -22,26 +21,19 @@
 */
 
 
-
-
-static int	ft_read()
-{
-
-
-}
-
 /*
  ** Stores the line on static array
  ** Returns 1 if there is more lines, 0 if end file.
 */ 
-static int	ft_next_line(int fd, int i, char *dst, char **src)
+static int	ft_next_line(char **lines, char **line, int fd, int r)
 {
 	int j;
 
-	j = 0;
-	while (line[j] != '\n')
+	size = 0;
+	while (line[fd][j] != '\n')
 	{
-		dst[i][j] = src[j];
+		while ()
+		lines[fd][j] = line[fd][j];
 		j++;
 	}
 	return (1);
@@ -52,52 +44,32 @@ static int	ft_next_line(int fd, int i, char *dst, char **src)
  ** First parameter is the file descriptor used to read, second parameter 
  ** is the adress of pointer to a character that will be used to save
  ** the line read from the file descriptor. 
- ** Returns -1 error, 0 rear completed, 1 line read. 
+ ** Returns -1 error, 0 read completed, 1 line read. 
  ** Functions allowed: libft, read(), malloc() and free().
  ** Bonus: use one single static variable, use multiple fd.
 */
 
 int		get_next_line(const int fd, char **line)
 {
-	int			r;
-	int			i;
-	static char	*lines[8192];
+	int			ret;
+	static char	*storage[8192];
 	
-	i = 0;
-	j = 0;
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || !line || BUFF_SIZE <= 0)
 		return (-1);
-	while (r = read(fd, line, BUFF_SIZE)
+	if (!storage[fd])
+		storage[fd] = ft_strnew(BUFF_SIZE);
+	*line = ft_strnew(0);
+	if (*storage[fd])
+		if (ft_next_line(storage[fd], line))
+			return (1);
+	while (ret = read(fd, storage[fd], BUFF_SIZE))
 	{
-		while (line[j] != '\n')
-		{
-			ft_next_line(fd, i, lines, line);
-		}
-		i++;
+		if (ret < 0)
+			return (-1);
+		if (ft_next_line(storage[fd], line))
+			return (1);
 	}
-
-
-}
-
-/*
- ** Easiest way using fgets, fputs and fclose
-*/
-int		main()
-{
-	static const char filename[] = "file.txt";
-	FILE *file = fopen ( filename, "r" );
-	if ( file != NULL )
-	{
-		char line [ 128 ]; /* or other suitable maximum line size */
-		while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
-		{
-			fputs ( line, stdout ); /* write the line */
-		}
-		fclose ( file );
-	}
-	else
-	{
-		perror ( filename ); /* why didn't the file open? */
-	}
-	return (0);
+	if (**line = 0)
+		return (1);
+	return (1);
 }

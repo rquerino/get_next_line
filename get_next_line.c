bba19d6824fd2cecccc611bd8c0a1d85d50f2b0c
Renato Querino <rquerino@e1z3r2p23.42.us.org>
2019-06-19 13:52:23 -0700

    final

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,69 @@
 /*   By: rquerino <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 11:55:17 by rquerino          #+#    #+#             */
-/*   Updated: 2019/06/08 13:16:39 by rquerino         ###   ########.fr       */
+/*   Updated: 2019/06/19 13:34:39 by rquerino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*
-** Checks if buff can reach end of file on fd's full size using
-** BUFF_SIZE on read(). Returns 1 if can't, 0 if ok.
-*/
-
-static int	ft_check_line(int fd, char **buff)
+static int	ft_read_line(char **str, char **line)
 {
-	int		r;
+	int		i;
 	char	*tmp;
-	char	*bckp;
 
-	tmp = ft_strnew(BUFF_SIZE + 1);
-	while ((r = read(fd, tmp, BUFF_SIZE)) > 0)
+	i = 0;
+	while ((*str)[i] != '\n' && (*str)[i] != '\0')
+		i++;
+	if ((*str)[i] == '\n')
 	{
-		bckp = buff[fd];
-		buff[fd] = ft_strjoin(buff[fd], tmp);
-		free(bckp);
-		if (!buff[fd])
-			ft_strdup(tmp);
-		ft_bzero(tmp, BUFF_SIZE);
+		*line = ft_strsub(*str, 0, i);
+		tmp = ft_strdup(&((*str)[i + 1]));
+		free(*str);
+		*str = tmp;
+		if ((*str)[0] == '\0')
+			ft_strdel(str);
 	}
-	free(tmp);
-	if (r)
-		return (1);
-	return (0);
+	else
+	{
+		*line = ft_strdup(*str);
+		ft_strdel(str);
+	}
+	return (1);
 }
 
-/*
-** Buff the fd's using BUFF_SIZE until '\n' is found, then returns 1.
-** If it's the end of file, returns 0.
-*/
-
-static int	ft_read_line(int fd, char **buff, char **line)
+static int	ft_return(int fd, int r, char **str, char **line)
 {
-	char	*tmp;
-	char	*bckp;
-
-	if ((tmp = ft_strchr(buff[fd], '\n')))
-	{
-		bckp = buff[fd];
-		*tmp = '\0';
-		*line = ft_strdup(buff[fd]);
-		buff[fd] = ft_strdup(tmp + 1);
-		free(bckp);
-		return (1);
-	}
-	if (*buff[fd])
-	{
-		*line = ft_strdup(buff[fd]);
-		buff[fd] = ft_strnew(BUFF_SIZE + 1);
-		return (1);
-	}
-	return (0);
+	if (r < 0)
+		return (-1);
+	else if (r == 0 && str[fd] == NULL)
+		return (0);
+	else
+		return (ft_read_line(&str[fd], line));
 }
-
-/*
-** Reads a file line by line.
-** First parameter is the file descriptor used to read, second parameter
-** is the adress of pointer to a character that will be used to save
-** the line read from the file descriptor.
-** Returns -1 error, 0 read completed, 1 line read.
-** Functions allowed: libft, read(), malloc() and free().
-** Bonus: use one single static variable, use multiple fd.
-*/
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	*buff[4864];
+	int			r;
+	static char	*str[4864];
+	char		buff[BUFF_SIZE + 1];
+	char		*tmp;
 
-	if (fd < 0 || !line || BUFF_SIZE <= 0 || ft_check_line(fd, &buff[fd]))
+	if (fd < 0 || !line || BUFF_SIZE <= 0)
 		return (-1);
-	if (ft_read_line(fd, &buff[fd], line))
-		return (1);
-	return (0);
+	while ((r = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[r] = '\0';
+		if (!str[fd])
+			str[fd] = ft_strdup(buff);
+		else
+		{
+			tmp = ft_strjoin(str[fd], buff);
+			free(str[fd]);
+			str[fd] = tmp;
+		}
+		if (ft_strchr(str[fd], '\n'))
+			break ;
+	}
+	return (ft_return(fd, r, str, line));
 }
